988a3cd51dae7b9f398896574d0623641732ce2f
Cho Seonghan <chosnhn1@gmail.com>
2025-03-11 17:46:02 +0900

    add signup / login / logout auth controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,15 @@
 import User from "../models/user.model.js";
 import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
   try {
     const {fullname, username, email, password} = req.body;
 
+    // if (!fullname || !username || !email || !password) {
+    //   return res.status(400).json({ error: "Please fill the form."})
+    // };
+    
     // Email Verification Regex
     // username part + @ + domain name part + .~ top-level domain part
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -17,6 +22,11 @@ export const signup = async (req, res) => {
     const existingEmail = await User.findOne({ username });
     if (existingEmail) {
       return res.status(400).json({ error: "Email is already taken."})
+    }
+
+    // Validation
+    if (password.length < 6) {
+      return res.status(400).json({ error: "Password cannot be shorter than 6"})
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -48,23 +58,43 @@ export const signup = async (req, res) => {
     }
 
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error."})
+    res.status(500).json({ error: error.message })
 
   }
-
-  res.json({
-    "data": "You hit the signup endpoint."
-  });
 };
 
 export const login = async (req, res) => {
-  res.json({
-    "data": "You hit the login endpoint."
-  });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if (!user || !isPasswordCorrect ){
+      return res.status(400).json({ error: "Invalid username or password."})
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      username: user.username,
+      email: user.email,
+      followers: user.followers,
+      followings: user.followings,
+      profileImg: user.profileImg,
+      coverImg: user.coverImg
+    });
+
+  } catch (error) {
+    return res.status(500).json({ error : "Internal server error."})
+  }
 };
 
 export const logout = async (req, res) => {
-  res.json({
-    "data": "You hit the logout endpoint."
-  });
+  try {
+    res.cookie("jwt", "", {maxAge: 0})
+    res.status(200).json({ message: "Logout performed successfully."})
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error."})
+  }
 };
