ad5ebc5f957b41f9d30b3786afcc09f840720560
Cho Seonghan <chosnhn1@gmail.com>
2025-03-18 18:10:11 +0900

    add create and delete post

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,74 @@
+import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
+
+export const createPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    let { img } = req.body;
+    const userId = req.user._id.toString();
+
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: "User not found."});
+
+    if (!text && !img) {
+      return res.status(400).json({ error: "Please write post before send it."})
+    }
+
+    if (img) {
+      // logic on upload img
+    }
+
+    let newPost = new Post({
+      user: userId,
+      text: text,
+      img: img
+    });
+    newPost = await newPost.save();
+    return res.status(201).json(newPost);
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+  
+};
+
+export const likeUnlikePost = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+  
+};
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({error: "Post not found."});
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res.status(401).json({error: "You are not authorized."});
+    }
+
+    if (post.img) {
+      // logic for delete(unassign) uploaded image
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+    return res.status(200).json({ message: "Post deleted."});
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+export const commentOnPost = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js"
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
