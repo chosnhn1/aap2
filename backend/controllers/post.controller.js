c895b111eb1a167e04c53d19d9a96175f6d58d8a
Cho Seonghan <chosnhn1@gmail.com>
2025-03-19 17:55:11 +0900

    add get all posts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -117,3 +117,17 @@ export const commentOnPost = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1});
+    if (posts.length === 0) {
+      return res.status(200).json([]);
+    }
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+};
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost, likeUnlikePost, commentOnPost, deletePost } from "../controllers/post.controller.js";
+import { createPost, likeUnlikePost, commentOnPost, deletePost, getAllPosts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
+router.get("/all", protectRoute, getAllPosts);
 router.post("/create", protectRoute, createPost);
 router.post("/like/:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
